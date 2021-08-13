ae285c4b5741dd89ef6e926ce0bec5e7db3725b5
youliangdao <youliangdaojing5@gmail.com>
2021-08-13 21:50:56 +0900

    head --ver2

--- head.c
+++ head.c
@@ -5,28 +5,48 @@ static void do_head(FILE *f, long nlines);
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2)
+    long nlines;
+    if (argc < 2)
     {
-        fprintf(stderr, "Usage: %s n\n");
+        fprintf(stderr, "Usage: %s n [file file ...]\n", argv[0]);
         exit(1);
     }
-    do_head(stdin, atol(argv[1]));
+
+    nlines = atol(argv[1]);
+    if (argc == 2)
+    {
+        do_head(stdin, nlines);
+    } else
+    {
+        for (int i = 2; i < argc; i++)
+        {
+            FILE *f;
+
+            f = fopen(argv[i], "r");
+            if (!f)
+            {
+                perror(argv[i]);
+                exit(1);
+            }
+            do_head(f, nlines);
+            
+        }
+        
+    }
     exit(0);
 }
 
-static void do_head(FILE *f, long nlines)
+static void
+do_head(FILE *f, long nlines)
 {
     int c;
 
     if (nlines <= 0) return;
-    while ((c = getc(f) != EOF))
-    {
+    while ((c = getc(f)) != EOF) {
         if (putchar(c) < 0) exit(1);
-        if (c == '\n') 
-        {
+        if (c == '\n') {
             nlines--;
             if (nlines == 0) return;
         }
     }
-    
 }
